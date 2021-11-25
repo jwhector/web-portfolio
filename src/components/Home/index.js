f518abfb125acd7dd335910d20c04defd5c0d82f
jwhector <jared.hector@gmail.com>
2021-11-24 22:08:50 -0800

    Add sticky to sidebar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,30 +1,52 @@
 import React, { useEffect } from 'react';
 import './index.css';
+import nameBox from './nameBox.png';
 
 export default function Home() {
     useEffect(() => {
         console.log(__dirname);
     });
 
+    // useEffect(() => {
+    //     const about = document.getElementById('about');
+    //     const portfolio = document.getElementById('portfolio');
+    //     const contact = document.getElementById('contact');
+
+    //     about.addEventListener()
+    // });
+
+    const scrollToAbout = () => {
+        const about = document.getElementById('about');
+        about.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+    const scrollToPortfolio = () => {
+        const portfolio = document.getElementById('portfolio');
+        portfolio.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+    const scrollToContact = () => {
+        const contact = document.getElementById('contact');
+        contact.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+
     return (
         <div id="home">
-            <h2 id="my-name" className="hdr-txt">Jared Hector</h2>
-            <div id="name-box">
-                
-            </div>   
-            <h3 id="subtitle">Developer. Project manager. Dungeon Master.</h3>
+            <div id="name">
+                <h2 id="my-name" className="hdr-txt">Jared Hector</h2>
+                <img id="name-box" alt="3D box surrounding 'Jared Hector'" src={nameBox} />
+            </div>
+            <h3 id="subtitle">Developer. Project Manager. Dungeon Master.</h3>
             <div className="main-links">
                 <div className="menu-div">
                     <h2 className="hdr-txt big-nav">ABOUT</h2>
-                    <a href="#about"></a>
+                    <a onClick={scrollToAbout}></a>
                 </div>
                 <div className="menu-div">
                     <h2 className="hdr-txt big-nav">PORTFOLIO</h2>
-                    <a href="#portfolio"></a>
+                    <a onClick={scrollToPortfolio}></a>
                 </div>
-                <div className="menu-div">
+                <div  className="menu-div">
                     <h2 className="hdr-txt big-nav">CONTACT</h2>
-                    <a href="#contact"></a>
+                    <a onClick={scrollToContact}></a>
                 </div>
             </div>
         </div>
